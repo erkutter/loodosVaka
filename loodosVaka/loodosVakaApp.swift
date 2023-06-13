5a2f7bd9f5ab5b5fda51f539a726d1e213439f35
erkutter <erkut.ter@bilgiedu.net>
2023-06-14 00:30:56 +0300

    Initial Commit

---
+++ loodosVaka/loodosVakaApp.swift
@@ -0,0 +1,17 @@
+//
+//  loodosVakaApp.swift
+//  loodosVaka
+//
+//  Created by Erkut Ter on 14.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct loodosVakaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
